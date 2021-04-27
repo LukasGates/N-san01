bf61e305026f4c19bc8728f4b35cd300d6308044
Lukas <lukas.gates@elev.ga.ntig.se>
2021-04-27 10:57:13 +0200

    skapande av nasan03

---
+++ näsan03/nasan03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nasan03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
