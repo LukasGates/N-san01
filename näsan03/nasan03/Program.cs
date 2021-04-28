eb56d91465cb73b5637cb76b3c4f7797eb0d867a
Lukas <lukas.gates@elev.ga.ntig.se>
2021-04-28 10:58:31 +0200

    3 första punkterna i näsan03 avklarade

--- näsan03/nasan03/Program.cs
+++ näsan03/nasan03/Program.cs
@@ -6,7 +6,38 @@ namespace nasan03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //punkt 1
+            Hello32();
+            static void Hello32()
+            {
+                int x = 0;
+                while (x < 32)
+                {
+                    Console.WriteLine("Hello, World");
+                    x++;
+                }
+
+            }
+
+            //Punkt 2
+            Gånger(6);
+            static void Gånger(int siffra)
+            {
+                int multiplikation = siffra * siffra;
+                Console.WriteLine(multiplikation);
+
+            }
+            
+            //Punkt 3
+            Console.WriteLine(Multi(1f, 2f));
+            static float Multi( float y, float z)
+            {
+                float multip = y * z;
+                return multip;
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
