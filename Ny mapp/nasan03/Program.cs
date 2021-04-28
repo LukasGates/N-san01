b7887b12f8c875f6e7994d192bbedc87e24152ff
Lukas <lukas.gates@elev.ga.ntig.se>
2021-04-28 10:13:07 +0200

    NyNasan03

---
+++ Ny mapp/nasan03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace nasan03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hey");
+        }
+    }
+}
